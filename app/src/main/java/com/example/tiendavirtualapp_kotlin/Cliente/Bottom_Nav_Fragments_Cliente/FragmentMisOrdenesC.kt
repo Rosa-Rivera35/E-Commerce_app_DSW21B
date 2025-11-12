a23d992b018ae2512367dc9924c59ecd94ef4dfa
E-Commerce_app_DSW21B <ri1122881@gmail.com>
2025-11-11 23:43:57 -0600

    Version3

--- app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/Bottom_Nav_Fragments_Cliente/FragmentMisOrdenesC.kt
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/Bottom_Nav_Fragments_Cliente/FragmentMisOrdenesC.kt
@@ -1,4 +1,4 @@
-package com.example.tiendavirtualapp_kotlin.Cliente.Nav_Fragments_Cliente
+package com.example.tiendavirtualapp_kotlin.Cliente.Bottom_Nav_Fragments_Cliente
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.tiendavirtualapp_kotlin.R
 
-class FragmentInicioC : Fragment() {
+class FragmentMisOrdenesC : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_c, container, false)
+        return inflater.inflate(R.layout.fragment_mis_ordenes_c, container, false)
     }
 
 }
--- app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/Nav_Fragments_Cliente/FragmentMiPerfilC.kt
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/Cliente/Nav_Fragments_Cliente/FragmentMiPerfilC.kt
@@ -7,14 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.tiendavirtualapp_kotlin.R
 
-class FragmentInicioC : Fragment() {
+class FragmentMiPerfilC : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio_c, container, false)
+        return inflater.inflate(R.layout.fragment_mi_perfil_c, container, false)
     }
 
 }
